b36766480c56191140df262d1cc03dfc37a7233c
Esteban Martini <emartini@comparaonline.com>
2016-10-25 19:31:40 -0300

    added css modules approach to react components

--- app/components/HelloWorld/index.js
+++ app/components/HelloWorld/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import styles from './styles.css';
+
+import Icon from '../Icon';
+import { BUBBLE } from '../Icon/constants';
 
 const HelloWorld = () => (
   <div>
-    <h1>Hello world!</h1>
+    <div className={styles.header}>
+      <h1>Hello world!</h1>
+    </div>
+
+    <div className={styles.body}>
+      <button>
+        <Icon icon={BUBBLE} color="black" />
+        Red
+      </button>
+
+      <button>
+        <Icon icon={BUBBLE} color="blue" />
+        Blue
+      </button>
+
+      <button className={styles.inProgress}>
+        <Icon icon={BUBBLE} color="red" />
+        { ' ' }
+        Red
+      </button>
+
+    </div>
+
+    <div className={styles.footer}>
+      Footer
+    </div>
   </div>
 );
 
